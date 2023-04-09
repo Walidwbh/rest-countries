077e8b331e542591f3d81d21a1dec0458b5f57c4
walidwbh <93353066+Walidwbh@users.noreply.github.com>
2023-04-09 17:28:47 +0100

    add countries when scrolling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CountriesService } from '../services/countries.service';
 import { Country } from '../types/country';
-import { Observable } from 'rxjs';
+import { Observable, delay, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,17 +10,29 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
   countries$!: Observable<Country[]>;
-  regions = [
-    {value: 'africa', viewValue: 'Africa'},
-    {value: 'america', viewValue: 'America'},
-    {value: 'asia', viewValue: 'Asia'},
-    {value: 'europe', viewValue: 'Europe'},
-    {value: 'oceania', viewValue: 'Oceania'},
-  ];
+  scrollSize: number = 16
+  countriesArray!: any[];
   constructor(private countrieService: CountriesService){
   }
   ngOnInit(): void {
-    this.countries$ = this.countrieService.getAllCountries();
-    // this.countries$.subscribe(res=>console.log(res));
+    this.getCountries(0);
+    this.countries$.subscribe(data =>this.countriesArray = data);
+  }
+  // get new 16 countries on scroll
+  @HostListener('window:scroll', ['$event'])
+  onScroll(){
+    let scrollHeight = document.documentElement.scrollHeight;
+    let scrollTop = document.documentElement.scrollTop ;
+    let clientHeight = document.documentElement.clientHeight;
+    let scrollPosition = scrollHeight - (scrollTop + clientHeight);
+    if(scrollPosition == 0 ) {
+      let oldScrollSize = this.scrollSize;
+      this.scrollSize += 16;
+      this.getCountries(oldScrollSize);
+      this.countries$.subscribe(data =>this.countriesArray.push(...data));
+    }
+  }
+  getCountries(l:number){
+    this.countries$ = this.countrieService.getAllCountries().pipe(map(x => x.slice(l,this.scrollSize)));
   }
 }
